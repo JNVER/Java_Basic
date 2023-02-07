8da48e7d0937040256d3a25d2e8beaea3f5b2733
JNVER <108576464+JNVER@users.noreply.github.com>
2023-02-07 10:15:17 +0900

    Create jdnsd.java

---
+++ sdks/jdnsd.java
@@ -0,0 +1,8 @@
+public class jdnsd {
+    public static void main(String[] args){
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
